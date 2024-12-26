6606ecd4a1ce648e67f5bc1ba4827b315086f60c
Shivam Chaurasiya <chshivam53@gmail.com>
2024-12-26 09:57:49 +0530

    updated

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-const Button = ({ icon, text }: { icon: any; text: string }) => {
+const Button = ({ icon, text }: { icon: React.ReactNode; text: string }) => {
   return (
     <div>
       <button className="bg-white hover:bg-black hover:text-white text-black rounded-full px-4 py-1 flex gap-2 items-center hover:border hover:border-white">
